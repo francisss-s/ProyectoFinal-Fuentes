3be9db94eef89b1d4e2fefa3e903904f9f9e844f
francisco.fuentes <FRANCISCO.FUENTES@WEST-INGENIERIA.CL>
2024-09-05 00:57:04 -0400

    Mejora rutas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,50 +7,49 @@ import HomePage from "./pages/HomePage";
 import OrderManagementPage from "./pages/OrderManagementPage";
 import ProductDetailPage from "./components/ProductDetailPage/ProductDetailPage";
 import ProductManagement from "./pages/ProductManagement";
-import ProductsPage  from "./pages/ProductsPage";
+import ProductsPage from "./pages/ProductsPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        
-        children: [
-            {
-                path: "/",
-                element: <Navigate to="/inicio" replace={true} />,
-            },
-            {
-                path: "/inicio",
-                element: <HomePage />,
-            },
-            {
-                path: "/productos",
-                element: <ProductsPage />,
-            },
-            {
-                path: "/productos/:id", // Ruta dinámica para el detalle del producto
-                element: <ProductDetailPage />,
-            },
-            {
-                path: "/gestionproductos",
-                element: <ProductManagement />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "/gestionOrdenes",
-                element: <OrderManagementPage />,
-            },
-            {
-                path: "/carrito",
-                element: <CartPage />,
-            }
-        ]
-    },
-    {
-    path: "*",
-    element: <Navigate to="/inicio" replace={true} />,
-    }
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,  // Usar index para la ruta raíz
+        element: <HomePage />,
+      },
+      {
+        path: "inicio",
+        element: <HomePage />,
+      },
+      {
+        path: "productos",
+        element: <ProductsPage />,
+      },
+      {
+        path: "productos/:id",
+        element: <ProductDetailPage />,
+      },
+      {
+        path: "gestionproductos",
+        element: <ProductManagement />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "gestionOrdenes",
+        element: <OrderManagementPage />,
+      },
+      {
+        path: "carrito",
+        element: <CartPage />,
+      },
+      {
+        path: "*",  // Manejo de rutas no encontradas
+        element: <Navigate to="/" replace />,
+      },
+    ],
+  },
 ]);
